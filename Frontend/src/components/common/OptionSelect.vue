59cfb3427ea057c64262e7aa7f3921828447346b
OSH <timber3@naver.com>
2023-11-20 15:53:49 +0900

    feat: fix gugun

--- Frontend/src/components/common/OptionSelect.vue
+++ Frontend/src/components/common/OptionSelect.vue
@@ -3,27 +3,27 @@ import { ref } from "vue";
 defineProps({ optionList: Array });
 const emit = defineEmits(["onKeySelect"]);
 
-const key = ref("");
+const gugun = ref("");
 
 const onSelect = () => {
-  console.log("21893789214361241837921372189378912" + key.value);
-  // console.log(key.value + "선택!!!");
-  emit("onKeySelect", key.value);
+    console.log("Option Select : key.value => " + gugun.value);
+    // console.log(key.value + "선택!!!");
+    emit("onKeySelect", gugun.value);
 };
 </script>
 
 <template>
-  <h1>Select.vue</h1>
-  <select v-model="key" class="form-select form-select-sm w-50" @change="onSelect">
-    <option
-      v-for="option in optionList"
-      :key="option.value"
-      :value="option.value"
-      :disabled="option.value === '' ? true : false"
-    >
-      {{ option.text }}
-    </option>
-  </select>
+    <h1>Select.vue</h1>
+    <select v-model="gugun" class="form-select form-select-sm w-50" @change="onSelect">
+        <option
+            v-for="option in optionList"
+            :key="option.value"
+            :value="option.value"
+            :disabled="option.value === '' ? true : false"
+        >
+            {{ option.text }}
+        </option>
+    </select>
 </template>
 
 <style scoped></style>
--- Frontend/src/views/TheTestView.vue
+++ Frontend/src/views/TheTestView.vue
@@ -11,102 +11,104 @@ const attractionList = ref([]);
 const attractionSelect = ref({});
 
 const param = ref({
-  pageNo: 1,
-  numOfRows: 20,
-  // zscode: 0,
-  sido: 0,
-  gugun: 0,
-  keyword: "가",
+    pageNo: 1,
+    numOfRows: 20,
+    // zscode: 0,
+    sido: 0,
+    gugun: 0,
+    keyword: "가",
 });
 
 onMounted(() => {
-  getSidoList();
+    getSidoList();
 });
 
 const getSidoList = () => {
-  listSido(
-    ({ data }) => {
-      console.log(data);
-      let options = [];
-      options.push({ text: "시도선택", value: "" });
-      data.forEach((sido) => {
-        options.push({ text: sido.sidoName, value: sido.sidoCode });
-      });
-      sidoList.value = options;
-      console.log("##############" + sidoList.value);
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+    listSido(
+        ({ data }) => {
+            console.log(data);
+            let options = [];
+            options.push({ text: "시도선택", value: "" });
+            data.forEach((sido) => {
+                options.push({ text: sido.sidoName, value: sido.sidoCode });
+            });
+            sidoList.value = options;
+            console.log("##############" + sidoList.value);
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
 };
 
 const onChangeSido = (val) => {
-  listGugun(
-    { sido: val },
-    ({ data }) => {
-      console.log(data);
-      let options = [];
-      options.push({ text: "구군선택", value: "" });
-      data.forEach((gugun) => {
-        options.push({ text: gugun.gugunName, value: gugun.gugunCode });
-      });
-      param.value.sido = val;
-      gugunList.value = options;
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+    listGugun(
+        { sido: val },
+        ({ data }) => {
+            console.log(data);
+            let options = [];
+            options.push({ text: "구군선택", value: "" });
+            data.forEach((gugun) => {
+                options.push({ text: gugun.gugunName, value: gugun.gugunCode });
+            });
+            param.value.sido = val;
+            gugunList.value = options;
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
 };
 
 const onChangeGugun = (val) => {
-  param.value.gugun = val;
-  console.log("kjSALFHDJAWKFawJKDFhAJKSDASL" + val);
-  getAttractionList();
+    param.value.gugun = val;
+    console.log("On change Gugun : val => " + val);
+    getAttractionList();
 };
 
 const getAttractionList = () => {
-  listAttraction(
-    param.value,
-    ({ data }) => {
-      console.log("data : " + data);
-      attractionList.value = data;
-      data.forEach((object) => {
-        console.log(object);
-        attractionList.value.push(object);
-      });
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+    listAttraction(
+        param.value,
+        ({ data }) => {
+            console.log("data : " + data);
+            attractionList.value = data;
+            data.forEach((object) => {
+                console.log(object);
+                attractionList.value.push(object);
+            });
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
 };
 
 const viewAttraction = (attraction) => {
-  attractionSelect.value = attraction;
+    attractionSelect.value = attraction;
 };
 </script>
 
 <template>
-  <h1>TheTestView.vue</h1>
-  <div class="container text-center mt-3">
-    <div class="alert alert-success" role="alert">관광지 정보</div>
-    <div class="row mb-2">
-      <div class="col d-flex flex-row-reverse">
-        <OptionSelect :optionList="sidoList" @onKeySelect="onChangeSido" />
-      </div>
-      <div class="col"><OptionSelect :optionList="gugunList" @onKeySelect="onChangeGugun" /></div>
-    </div>
-    <TestMap :attractionList="attractionList" :attractionSelect="attractionSelect" />
-    <table class="table table-hover">
-      <thead>
-        <tr class="text-center">
-          <!-- <th scope="col">관광지 상세/th></th> -->
-        </tr>
-      </thead>
-      <tbody>
-        <!-- <tr
+    <h1>TheTestView.vue</h1>
+    <div class="container text-center mt-3">
+        <div class="alert alert-success" role="alert">관광지 정보</div>
+        <div class="row mb-2">
+            <div class="col d-flex flex-row-reverse">
+                <OptionSelect :optionList="sidoList" @onKeySelect="onChangeSido" />
+            </div>
+            <div class="col">
+                <OptionSelect :optionList="gugunList" @onKeySelect="onChangeGugun" />
+            </div>
+        </div>
+        <TestMap :attractionList="attractionList" :attractionSelect="attractionSelect" />
+        <table class="table table-hover">
+            <thead>
+                <tr class="text-center">
+                    <!-- <th scope="col">관광지 상세/th></th> -->
+                </tr>
+            </thead>
+            <tbody>
+                <!-- <tr
           class="text-center"
           v-for="attraction in attractionList"
           :key="attraction.statId + attraction.chgerId"
@@ -114,13 +116,13 @@ const viewAttraction = (attraction) => {
         >
           <th>{{ attraction.statId }}</th>
         </tr> -->
-      </tbody>
-    </table>
-  </div>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style scoped>
 mark.purple {
-  background: linear-gradient(to top, #c354ff 20%, transparent 30%);
+    background: linear-gradient(to top, #c354ff 20%, transparent 30%);
 }
 </style>
