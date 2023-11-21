63b4e9502caa3ac94839f1dc1e44e1a7fa142db9
hojo <ubermensch100326@gmail.com>
2023-11-21 15:08:28 +0900

    feat: add map marker and overlay

--- Frontend/src/components/common/TestMap.vue
+++ Frontend/src/components/common/TestMap.vue
@@ -2,165 +2,214 @@
 import { ref, watch, onMounted } from "vue";
 
 var map;
-var overlay = {};
-var overlays = [];
-const positions = ref([]);
+var positions = [];
 const markers = ref([]);
+const overlays = ref([]);
 
 const props = defineProps({ attractionList: Array, attractionSelect: Object });
 
 watch(
-    () => props.attractionList.value,
-    () => {
-        positions.value = [];
-        props.attractionList.forEach((attraction) => {
-            let obj = {};
-            obj.latlng = new kakao.maps.LatLng(attraction.latitude, attraction.longitude);
-            obj.title = attraction.statNm;
+  () => props.attractionList.value,
+  () => {
+    deleteMarkers();
+    positions = [];
+    props.attractionList.forEach((attraction) => {
+      let obj = {};
+      obj.latlng = new kakao.maps.LatLng(attraction.latitude, attraction.longitude);
+      obj.title = attraction.title;
+      obj.addr = attraction.addr1;
+      obj.first_image = attraction.first_image;
+      obj.tel = attraction.tel;
+      positions.push(obj);
+    });
 
-            positions.value.push(obj);
-        });
-        loadMarkers();
-    },
-    { deep: true }
+    for (let i = 0; i < positions.length; i++) {
+      var data = positions[i];
+      loadMarkers(data);
+    }
+  },
+  { deep: true }
 );
 
 watch(
-    () => props.attractionSelect.value,
-    () => {
-        // 이동할 위도 경도 위치를 생성합니다
-        var moveLatLon = new kakao.maps.LatLng(
-            props.attractionSelect.latitude,
-            props.attractionSelect.longitude
-        );
-        // 지도 중심을 부드럽게 이동시킵니다
-        // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-        map.panTo(moveLatLon);
-    },
-    { deep: true }
+  () => props.attractionSelect.value,
+  () => {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(
+      props.attractionSelect.latitude,
+      props.attractionSelect.longitude
+    );
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(moveLatLon);
+  },
+  { deep: true }
 );
 
 onMounted(() => {
-    if (window.kakao && window.kakao.maps) {
-        initMap();
-    } else {
-        const script = document.createElement("script");
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${
-            import.meta.env.VITE_KAKAO_MAP_SERVICE_KEY
-        }&libraries=services,clusterer`;
-        /* global kakao */
-        script.onload = () => kakao.maps.load(() => initMap());
-        document.head.appendChild(script);
-    }
+  if (window.kakao && window.kakao.maps) {
+    initMap();
+  } else {
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${
+      import.meta.env.VITE_KAKAO_MAP_SERVICE_KEY
+    }&libraries=services,clusterer`;
+    /* global kakao */
+    script.onload = () => kakao.maps.load(() => initMap());
+    document.head.appendChild(script);
+  }
 });
 
 const initMap = () => {
-    const container = document.getElementById("map");
-    console.log(container);
-    var options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
-    };
+  const container = document.getElementById("map");
+  console.log(container);
+  var options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3,
+  };
 
-    map = new kakao.maps.Map(container, options);
-    console.log(map);
-    // loadMarkers();
+  map = new kakao.maps.Map(container, options);
+  console.log(map);
+  // loadMarkers();
 };
 
-function closeOverlay() {
-    console.log("closeOverLay 호출");
+// function closeOverlay() {
+//   console.log("closeOverLay 호출");
+//   overlay.setMap(null);
+// }
+
+const loadMarkers = (position) => {
+  var marker = new kakao.maps.Marker({
+    map: map, // 마커를 표시할 지도
+    position: position.latlng, // 마커를 표시할 위치
+    // title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
+    clickable: true, // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+    // image: markerImage, // 마커의 이미지
+  });
+
+  // 마커 배열에 각 마커 추가
+  markers.value.push(marker);
+
+  ///////////////////////////////////////////////////
+  var content = document.createElement("div");
+  content.classList.add("wrap");
+  content.style.backgroundColor = "white";
+
+  var infoDiv = document.createElement("div");
+  infoDiv.classList.add("info");
+
+  var titleDiv = document.createElement("div");
+  titleDiv.classList.add("title");
+  titleDiv.style.backgroundColor = "gray";
+  titleDiv.textContent = position.title;
+
+  var closeButton = document.createElement("button");
+  closeButton.classList.add("close");
+  closeButton.setAttribute("title", "닫기");
+  //   closeButton.textContent = "x";
+  closeButton.onclick = closeBtn; // closeOverlay 함수를 연결합니다.
+
+  titleDiv.appendChild(document.createTextNode(" "));
+  titleDiv.appendChild(closeButton);
+
+  var bodyDiv = document.createElement("div");
+  bodyDiv.classList.add("body");
+
+  var imgDiv = document.createElement("div");
+  imgDiv.classList.add("img");
+
+  var image = document.createElement("img");
+  image.setAttribute("src", "/src/assets/noimage.png");
+  image.setAttribute("width", "73");
+  image.setAttribute("height", "70");
+  if (position.first_image !== "") image.setAttribute("src", position.first_image);
+  //position.first_image
+
+  imgDiv.appendChild(image);
+
+  var descDiv = document.createElement("div");
+  descDiv.classList.add("desc");
+
+  var ellipsisDiv = document.createElement("div");
+  ellipsisDiv.classList.add("ellipsis");
+  ellipsisDiv.textContent = position.addr;
+
+  var linkDiv = document.createElement("div");
+  var paragraph = document.createElement("p");
+  paragraph.textContent = position.tel;
+  linkDiv.appendChild(paragraph);
+
+  descDiv.appendChild(ellipsisDiv);
+  descDiv.appendChild(linkDiv);
+
+  bodyDiv.appendChild(imgDiv);
+  bodyDiv.appendChild(descDiv);
+
+  infoDiv.appendChild(titleDiv);
+  infoDiv.appendChild(bodyDiv);
+
+  content.appendChild(infoDiv);
+  ////////////////////////////////////////
+
+  var overlay = new kakao.maps.CustomOverlay({
+    content: content,
+    clickable: true,
+    xAnchor: 0.5,
+    yAnchor: 1.3,
+    zIndex: 3,
+    position: marker.getPosition(),
+  });
+
+  var closeBtn = document.createElement("button");
+  closeBtn.innerHTML = "닫기";
+  closeBtn.onclick = function () {
     overlay.setMap(null);
-}
+  };
 
-const loadMarkers = () => {
-    // 현재 표시되어있는 marker들이 있다면 map에 등록된 marker를 제거한다.
-    deleteMarkers();
+  content.appendChild(closeBtn);
+  overlay.setContent(content);
 
-    // 마커를 생성합니다
-    markers.value = [];
-    console.log("position is => " + positions.value);
-    positions.value.forEach((position) => {
-        const marker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: position.latlng, // 마커를 표시할 위치
-            // title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
-            clickable: true, // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-            // image: markerImage, // 마커의 이미지
-        });
+  kakao.maps.event.addListener(marker, "click", function () {
+    overlay.setMap(map);
+  });
+  // overlays.push(overlay);
 
-        // 마커 배열에 각 마커 추가
-        markers.value.push(marker);
-
-        var content =
-            '<div class="wrap" style="background-color: white;">' +
-            '    <div class="info">' +
-            '        <div class="title" style="background-color: gray;">' +
-            position.title +
-            "             " +
-            '            <button class="close" onclick="closeOverlay()" title="닫기">x</button>' +
-            "        </div>" +
-            '        <div class="body">' +
-            '            <div class="img">' +
-            '                <img src="' +
-            position.first_image +
-            '" width="73" height="70">' +
-            "           </div>" +
-            '            <div class="desc">' +
-            '                <div class="ellipsis">' +
-            position.addr1 +
-            "</div>" +
-            '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-            '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
-            "            </div>" +
-            "        </div>" +
-            "    </div>" +
-            "</div>";
-
-        overlay = new kakao.maps.CustomOverlay({
-            content: content,
-            map: map,
-            clickable: true,
-            xAnchor: 0.5,
-            yAnchor: 1.3,
-            zIndex: 3,
-            position: marker.getPosition(),
-        });
-
-        overlay.setContent(content);
-        // overlays.push(overlay);
+  // 마커 배열의 마커들에게 클릭 이벤트 추가 하는 부분.
+  markers.value.forEach((marker) => {
+    kakao.maps.event.addListener(marker, "click", function () {
+      overlays.value.setMap(map);
     });
+  });
 
-    // 마커 배열의 마커들에게 클릭 이벤트 추가 하는 부분.
-    markers.value.forEach((marker) => {
-        kakao.maps.event.addListener(marker, "click", function () {
-            overlays.setMap(map);
-        });
-    });
+  // 4. 지도를 이동시켜주기
+  // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
+  const bounds = positions.reduce(
+    (bounds, position) => bounds.extend(position.latlng),
+    new kakao.maps.LatLngBounds()
+  );
 
-    // 4. 지도를 이동시켜주기
-    // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
-    const bounds = positions.value.reduce(
-        (bounds, position) => bounds.extend(position.latlng),
-        new kakao.maps.LatLngBounds()
-    );
+  overlays.value.push(overlay);
 
-    map.setBounds(bounds);
+  map.setBounds(bounds);
 };
 
 const deleteMarkers = () => {
-    if (markers.value.length > 0) {
-        markers.value.forEach((marker) => marker.setMap(null));
-    }
+  if (markers.value.length > 0) {
+    markers.value.forEach((marker) => marker.setMap(null));
+  }
+  if (overlays.value.length > 0) {
+    overlays.value.forEach((overlay) => overlay.setMap(null));
+  }
 };
 </script>
 
 <template>
-    <div id="map"></div>
+  <div id="map"></div>
 </template>
 
 <style scoped>
 #map {
-    width: 100%;
-    height: 700px;
+  width: 100%;
+  height: 700px;
 }
 </style>
--- Frontend/src/views/TheTestView.vue
+++ Frontend/src/views/TheTestView.vue
@@ -8,19 +8,19 @@ import OptionSelect from "@/components/common/OptionSelect.vue";
 const props = defineProps({ selectedAttraction: Object });
 
 watch(
-    () => props.selectedAttraction.value,
-    () => {
-        // 이동할 위도 경도 위치를 생성합니다
-        var moveLatLon = new kakao.maps.LatLng(
-            props.selectedAttraction.lat,
-            props.selectedAttraction.lng
-        );
+  () => props.selectedAttraction.value,
+  () => {
+    // 이동할 위도 경도 위치를 생성합니다
+    var moveLatLon = new kakao.maps.LatLng(
+      props.selectedAttraction.lat,
+      props.selectedAttraction.lng
+    );
 
-        // 지도 중심을 부드럽게 이동시킵니다
-        // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-        map.panTo(moveLatLon);
-    },
-    { deep: true }
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(moveLatLon);
+  },
+  { deep: true }
 );
 
 const sidoList = ref([]);
@@ -31,147 +31,149 @@ const keyword = ref("");
 const type = ref("");
 
 const param = ref({
-    pageNo: 1,
-    numOfRows: 20,
-    // zscode: 0,
-    sido: 0,
-    gugun: 0,
-    keyword: "가",
-    typeNum: 12,
+  pageNo: 1,
+  numOfRows: 20,
+  // zscode: 0,
+  sido: 0,
+  gugun: 0,
+  keyword: "가",
+  typeNum: 12,
 });
 
 onMounted(() => {
-    getSidoList();
+  getSidoList();
 });
 
 const getSidoList = () => {
-    listSido(
-        ({ data }) => {
-            console.log(data);
-            let options = [];
-            options.push({ text: "시도선택", value: "" });
-            data.forEach((sido) => {
-                options.push({ text: sido.sidoName, value: sido.sidoCode });
-            });
-            sidoList.value = options;
-            console.log("##############" + sidoList.value);
-        },
-        (err) => {
-            console.log(err);
-        }
-    );
+  listSido(
+    ({ data }) => {
+      console.log(data);
+      let options = [];
+      options.push({ text: "시도선택", value: "" });
+      data.forEach((sido) => {
+        options.push({ text: sido.sidoName, value: sido.sidoCode });
+      });
+      sidoList.value = options;
+      console.log("##############" + sidoList.value);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
 };
 
 const onChangeSido = (val) => {
-    listGugun(
-        { sido: val },
-        ({ data }) => {
-            console.log(data);
-            let options = [];
-            options.push({ text: "구군선택", value: "" });
-            data.forEach((gugun) => {
-                options.push({ text: gugun.gugunName, value: gugun.gugunCode });
-            });
-            param.value.sido = val;
-            gugunList.value = options;
-        },
-        (err) => {
-            console.log(err);
-        }
-    );
+  listGugun(
+    { sido: val },
+    ({ data }) => {
+      console.log(data);
+      let options = [];
+      options.push({ text: "구군선택", value: "" });
+      data.forEach((gugun) => {
+        options.push({ text: gugun.gugunName, value: gugun.gugunCode });
+      });
+      param.value.sido = val;
+      gugunList.value = options;
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
 };
 
 const onChangeGugun = (val) => {
-    param.value.gugun = val;
-    console.log("On change Gugun : val => " + val);
+  param.value.gugun = val;
+  console.log("On change Gugun : val => " + val);
 };
 
 const onChangeType = () => {
-    param.value.typeNum = type.value;
-    console.log("On change type : val => " + type.value);
+  param.value.typeNum = type.value;
+  console.log("On change type : val => " + type.value);
 };
 
 // const onChangeType
 
 const onSearchButtonClick = () => {
-    console.log(11111);
-    param.value.keyword = keyword.value;
-    getAttractionList();
+  console.log(11111);
+  param.value.keyword = keyword.value;
+  getAttractionList();
 };
 
 const getAttractionList = () => {
-    listAttraction(
-        param.value,
-        ({ data }) => {
-            console.log("data : " + data);
-            attractionList.value = data;
-            console.log("##################" + attractionList.value);
-        },
-        (err) => {
-            console.log(err);
-        }
-    );
+  listAttraction(
+    param.value,
+    ({ data }) => {
+      console.log("data : " + data);
+      attractionList.value = data;
+      console.log("##################" + attractionList.value);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
 };
 
 const viewAttraction = (attraction) => {
-    console.log("@@@@@@@@@@@@@@  viewAttraction change => " + attraction.attraction_id);
-    attractionSelect.value = attraction;
+  console.log("@@@@@@@@@@@@@@  viewAttraction change => " + attraction.attraction_id);
+  attractionSelect.value = attraction;
 };
 </script>
 
 <template>
-    <h1>TheTestView.vue</h1>
-    <div class="container text-center mt-3">
-        <div class="alert alert-success" role="alert">관광지 정보</div>
-        <div class="row mb-2">
-            <div class="col">
-                <OptionSelect :optionList="sidoList" @onKeySelect="onChangeSido" />
-            </div>
-            <div class="col">
-                <OptionSelect :optionList="gugunList" @onKeySelect="onChangeGugun" />
-            </div>
-            <div class="col">
-                <select @change="onChangeType" v-model="type">
-                    <option value="12">관광지</option>
-                    <option value="14">문화시설</option>
-                    <option value="15">축제공연행사</option>
-                    <option value="25">여행코스</option>
-                    <option value="28">레포츠</option>
-                    <option value="32">숙박</option>
-                    <option value="38">쇼핑</option>
-                    <option value="39">음식점</option>
-                </select>
-            </div>
-            <div class="col">
-                <input type="text" v-model="keyword" @keyup.enter="onSearchButtonClick" />
-                <button @click="onSearchButtonClick">검색</button>
-            </div>
-        </div>
-        <TestMap :attractionList="attractionList" :attractionSelect="attractionSelect" />
-        <table class="table table-hover">
-            <thead>
-                <tr class="text-center">
-                    <!-- <th scope="col">관광지 상세/th></th> -->
-                </tr>
-            </thead>
-            <tbody>
-                <tr
-                    class="text-center"
-                    v-for="attraction in attractionList"
-                    :key="attraction.attraction_id"
-                    @click="viewAttraction(attraction)"
-                >
-                    <th><img :src="attraction.first_image" width="50" /></th>
-                    <th>{{ attraction.attraction_id }}</th>
-                    <th>{{ attraction.title }}</th>
-                </tr>
-            </tbody>
-        </table>
+  <h1>TheTestView.vue</h1>
+  <div class="container text-center mt-3">
+    <div class="alert alert-success" role="alert">관광지 정보</div>
+    <div class="row mb-2">
+      <div class="col">
+        <OptionSelect :optionList="sidoList" @onKeySelect="onChangeSido" />
+      </div>
+      <div class="col">
+        <OptionSelect :optionList="gugunList" @onKeySelect="onChangeGugun" />
+      </div>
+      <div class="col">
+        <select @change="onChangeType" v-model="type">
+          <option value="12">관광지</option>
+          <option value="14">문화시설</option>
+          <option value="15">축제공연행사</option>
+          <option value="25">여행코스</option>
+          <option value="28">레포츠</option>
+          <option value="32">숙박</option>
+          <option value="38">쇼핑</option>
+          <option value="39">음식점</option>
+        </select>
+      </div>
+      <div class="col">
+        <input type="text" v-model="keyword" @keyup.enter="onSearchButtonClick" />
+        <button @click="onSearchButtonClick">검색</button>
+      </div>
     </div>
+    <Suspense
+      ><TestMap :attractionList="attractionList" :attractionSelect="attractionSelect"
+    /></Suspense>
+    <table class="table table-hover">
+      <thead>
+        <tr class="text-center">
+          <!-- <th scope="col">관광지 상세/th></th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="text-center"
+          v-for="attraction in attractionList"
+          :key="attraction.attraction_id"
+          @click="viewAttraction(attraction)"
+        >
+          <th><img :src="attraction.first_image" width="50" /></th>
+          <th>{{ attraction.attraction_id }}</th>
+          <th>{{ attraction.title }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
 mark.purple {
-    background: linear-gradient(to top, #c354ff 20%, transparent 30%);
+  background: linear-gradient(to top, #c354ff 20%, transparent 30%);
 }
 </style>
